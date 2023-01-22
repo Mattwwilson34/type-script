d7d97a8fb92ba0d75ed5fdd1240d1eb313bc08d2
Matt Wilson <mattwwilson34@gmail.com>
2023-01-21 19:51:50 -0500

    Add: sandbox files

---
+++ sandbox/script.js
@@ -0,0 +1,8 @@
+function hello(s) {
+    console.log(s);
+}
+hello('hi this is my first typescript sandbox');
+console.log('testing compile on save');
+console.log('testing compile on save');
+console.log('testing compile on save');
+console.log('testing compile on save');
---
+++ sandbox/script.ts
@@ -0,0 +1,10 @@
+function hello(s: string) {
+	console.log(s);
+}
+hello('hi this is my first typescript sandbox');
+
+console.log('testing compile on save');
+console.log('testing compile on save');
+console.log('testing compile on save');
+console.log('testing compile on save');
+
